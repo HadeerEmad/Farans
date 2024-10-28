4560ab70ff7acc77d7f36fc9477da8ff24c81e32
Hadeer Emad <23742626+HadeerEmad@users.noreply.github.com>
2024-10-29 00:57:04 +0300

    email

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+    $name = $_POST['name'];
+    $service = $_POST['service'];
+    $subject = $_POST['title'];
+    $message = $_POST['message'];
+
+    $formContent = "From: $name \n\nRequsted Service: $service  \n\nProject Idea: $subject  \n\nConnect Way: $message";
+
+    $recipient = "support@farans.io";
+    $mailHeader = "From: $name \r\n";
+    mail($recipient, $subject, $formContent, $mailHeader) or die("Error!");
+    header('Location: index.html');
+
+?>
