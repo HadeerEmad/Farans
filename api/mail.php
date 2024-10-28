e374a2a451d1dcf60959a744aab6c2f21dbe1c1c
Hadeer Emad <23742626+HadeerEmad@users.noreply.github.com>
2024-10-29 01:45:08 +0300

    php

--- api/mail.php
+++ api/mail.php
@@ -1,5 +1,8 @@
 <?php
 
+    header('content-type: application/json');
+    echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+
     $name = $_POST['name'];
     $service = $_POST['service'];
     $subject = $_POST['title'];
